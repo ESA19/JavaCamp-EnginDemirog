24a4494bf953a04d6c8ba4686639f7630aaf0d77
Enes Samet Aydı <92991460+ESA19@users.noreply.github.com>
2022-09-20 11:37:31 +0300

    Add files via upload

---
+++ switchDemo/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char grade='A';
+        switch (grade)
+        {
+            case 'A':
+                System.out.println("Mükemmel : Geçtiniz");
+                break;
+            case 'B':
+                System.out.println("İyi : Geçtiniz");
+                break;
+            case 'C':
+                System.out.println("Orta : Geçtiniz");
+                break;
+            case 'D':
+                System.out.println("Kötü : Kaldınız");
+                break;
+        }
+    }
+}
