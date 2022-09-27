928b8bff236424d5354483c5431c4c8b3e1be174
Enes Samet Aydı <esa.aydi@gmail.com>
2022-09-27 11:47:27 +0300

    Update

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,5 +1,40 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException{
+        Connection connection=null;
+        DbHelper dbHelper= new DbHelper();
+        Statement statement=null;
+        ResultSet resultSet;
+
+        try {
+
+            connection=dbHelper.getConnection();
+            System.out.println("Bağlantı Oluştu");
+            statement=connection.createStatement();
+            resultSet=statement.executeQuery("select * from actor ");
+            ArrayList<Actor> actors=new ArrayList<Actor>();
+            String s;
+            while (resultSet.next()){
+                actors.add(new Actor(resultSet.getString("first_Name"),resultSet.getString("last_Name")));
+
+            }
+            for (int i = 0; i < actors.size(); i++) {
+               s=actors.get(i).toString();
+
+                System.out.println(s);
+            }
+
+        }
+        catch (SQLException e){
+            dbHelper.showErrorMessage(e);
+
+        }
+        finally {
+            connection.close();
+        }
 
     }
 }
